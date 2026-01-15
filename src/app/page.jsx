11a68e408791649c2201d7d2f5dd5332ca766ab4
santimontironi <santiimontironi@gmail.com>
2026-01-14 22:32:26 -0300

    estilos en Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,27 @@
+import About from "./components/landing/About"
+import Services from "./components/landing/Services"
+import Home from "./components/landing/Home"
+import Contact from "./components/landing/Contact"
+
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <section id="inicio">
+        <Home/>
+      </section>
+
+      <section id="sobre-nosotros">
+        <About/>
+      </section>
+
+      <section id="servicios">
+        <Services/>
+      </section>
+
+      <section id="contacto">
+        <Contact/>
+      </section>
+    </>
   )
 }
 
